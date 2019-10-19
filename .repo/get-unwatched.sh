662f27e156bc524f13a5559e8e765d990f4b7336
Morten Linderud <morten@linderud.pw>
2019-10-19 12:51:27 +0200

    Fixed .repo tools

--- .repo/get-unwatched.sh
+++ .repo/get-unwatched.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 readonly MAINTAINER="Foxboron"
 
-blacklist=( __config__ go-tools python2-docs)
+blacklist=( __config__ go-tools python2-docs minicom)
 
-sort <(curl -s "https://www.archlinux.org/packages/search/json/?sort=&q=&maintainer=$MAINTAINER&flagged=" | jq -r '.results[].pkgbase') <(grep -oP '\[\K[^\]]+' .repo/packages.ini) <(printf "%s\n" "${blacklist[@]}") | uniq -u
+sort <(curl -s "https://www.archlinux.org/packages/search/json/?sort=&q=&maintainer=$MAINTAINER&flagged=" | jq -r '.results[].pkgbase') <(grep -oP '^\[\K[^\]]+' .repo/packages.ini) <(printf "%s\n" "${blacklist[@]}") | uniq -u
