f2a08728ff069e9c7893901d202bce7f80796e29
Morten Linderud <morten@linderud.pw>
2019-11-23 13:10:57 +0100

    [.repo] Misc stuff has changed
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- .repo/get-unwatched.sh
+++ .repo/get-unwatched.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 readonly MAINTAINER="Foxboron"
 
-blacklist=( __config__ go-tools python2-docs minicom)
+blacklist=( __config__ go-tools python2-docs minicom perl-anyevent-i3)
 
 sort <(curl -s "https://www.archlinux.org/packages/search/json/?sort=&q=&maintainer=$MAINTAINER&flagged=" | jq -r '.results[].pkgbase') <(grep -oP '^\[\K[^\]]+' .repo/packages.ini) <(printf "%s\n" "${blacklist[@]}") | uniq -u
