065c7ce7afedebb0a11eecf72ec653a1c54d789e
Morten Linderud <morten@linderud.pw>
2020-10-18 15:37:21 +0200

    .repo changes
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- .repo/get-current-versions.sh
+++ .repo/get-current-versions.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 while IFS= read -r -d '' srcinfo; do
-	printf "%s %s\n" "$(awk '/pkgbase/ {print $3}' "$srcinfo")" "$(awk '/pkgver/ {print $3}' "$srcinfo")"
+	if ! git show ":$srcinfo" &>/dev/null; then
+		continue
+	fi
+	printf "%s %s\n" "$(awk '/pkgbase/ {print $3}' <(git show ":$srcinfo"))" "$(awk '/pkgver/ {print $3}' <(git show ":$srcinfo"))"
 done < <(find . -maxdepth 2 -name ".SRCINFO" -print0)
