b1ebe2fc4a7a1350bd78d12b812cc0a29c0d1262
Morten Linderud <morten@linderud.pw>
2020-11-08 13:31:16 +0100

    repo: Fix meta stuff
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- .repo/get-unwatched.sh
+++ .repo/get-unwatched.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 readonly MAINTAINER="Foxboron"
 
-blacklist=( __config__ go-tools python2-docs minicom perl-anyevent-i3)
+blacklist=( __config__)
 
 sort <(curl -s "https://www.archlinux.org/packages/search/json/?sort=&q=&maintainer=$MAINTAINER&flagged=" | jq -r '.results[].pkgbase' | sort | uniq) <(grep -oP '^\[\K[^\]]+' .repo/packages.toml) <(printf "%s\n" "${blacklist[@]}") | uniq -u
