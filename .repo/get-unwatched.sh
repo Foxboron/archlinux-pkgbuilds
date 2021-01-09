746c8df0ed33becaa0c603409a1aed04c962301e
Morten Linderud <morten@linderud.pw>
2021-01-09 16:08:04 +0100

    dotrepo changes
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- .repo/get-unwatched.sh
+++ .repo/get-unwatched.sh
@@ -3,4 +3,4 @@ readonly MAINTAINER="Foxboron"
 
 blacklist=( __config__)
 
-sort <(curl -s "https://www.archlinux.org/packages/search/json/?sort=&q=&maintainer=$MAINTAINER&flagged=" | jq -r '.results[].pkgbase' | sort | uniq) <(grep -oP '^\[\K[^\]]+' .repo/packages.toml) <(printf "%s\n" "${blacklist[@]}") | uniq -u
+sort <(curl -s "https://archlinux.org/packages/search/json/?sort=&q=&maintainer=$MAINTAINER&flagged=" | jq -r '.results[].pkgbase' | sort | uniq) <(grep -oP '^\[\K[^\]]+' .repo/packages.toml) <(printf "%s\n" "${blacklist[@]}") | uniq -u
